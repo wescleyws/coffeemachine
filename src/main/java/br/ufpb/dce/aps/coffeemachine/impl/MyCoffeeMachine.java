2cd9c3bf7b32245b2f8632917c5854853dda1a78
Aluno <me@me.com>
2014-07-15 09:47:07 -0300

    Task 1.02 finished for COMOPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,13 +1,24 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import net.compor.frameworks.jcf.api.ComporFacade;
 
 public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine{
 
+	private int totalcentavos = 0;
+	private ComponentsFactory fac;
+	
 	public MyCoffeeMachine(ComponentsFactory factory) {
-		factory.getDisplay().info("Insert coins and select a drink!");
+		fac = factory;
+		fac.getDisplay().info("Insert coins and select a drink!");
+	}
+
+	public void insertCoin(Coin coin) {
+		totalcentavos += coin.getValue();
+		fac.getDisplay().info("Total: US$ " + totalcentavos/100 + "." + totalcentavos%100 );
+		
 	}
 	
 	
